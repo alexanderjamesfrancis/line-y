869d92444290b38f4c06cb05c8f574ce8f06fb72
Alex Francis <alexanderjamesfrancis@hotmail.co.uk>
2023-04-12 13:05:57 +0100

    Border Changes

--- componants/buttons/LandingButton.js
+++ componants/buttons/LandingButton.js
@@ -1,7 +1,7 @@
 export default function LandingButton ({children, action}) {
     return(
     <div className="flex justify-center">
-    <button onClick={action} className="w-[60%] h-12 my-2 rounded-lg border-yellow-600 bg-yellow-500 border border-black">
+    <button onClick={action} className="w-[60%] h-12 my-2 rounded-lg border-yellow-600 bg-yellow-500 border">
         {children}
     </button>
     </div>
